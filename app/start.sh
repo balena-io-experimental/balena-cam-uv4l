74d96e05a28e3bc1c328862ee83399b19089a7cc
Nick Stathas <nickstathas@outlook.com>
2017-07-05 17:01:57 +0300

    Various Corrections

--- app/start.sh
+++ app/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Start turn server as daemon if enabled
-# [ ! -z ${disable_turn+x} ] || turnserver --no-stun --no-cli --no-multicast-peers -o
-
 source ./set_option.sh
 
 # Reconfigure uv4l.conf
@@ -10,8 +7,8 @@ set_option height $height /usr/src/app/config/uv4l.conf
 set_option width $width /usr/src/app/config/uv4l.conf
 set_option framerate $framerate /usr/src/app/config/uv4l.conf
 
-# Reconfigure the uv4l service
+# Reconfigure the UV4L service
 sed -i 's^/etc/uv4l/uv4l-raspicam.conf^/usr/src/app/config/uv4l.conf^g' /etc/init.d/uv4l_raspicam
 
-# Start uv4l
+# Start UV4L
 systemctl start uv4l_raspicam
